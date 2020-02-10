c09ae33aa7127f0541a32f156900588d7a779485
Joe <maddo4.dev@gmail.com>
2020-02-10 16:36:21 +0000

    Added test classes and staff class

---
+++ HardwareTesting/tstStaff.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HardwareClasses;
+
+namespace HardwareTesting
+{
+    [TestClass]
+    public class tstStaff
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsStaff staffMember = new clsStaff();
+            Assert.IsNotNull(staffMember);
+        }
+    }
+}
