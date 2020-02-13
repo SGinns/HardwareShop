23a0e796db846a2d9cdb502b9204f1ab83692b88
Joseph Edwards <P2414379@my365.dmu.ac.uk>
2020-02-13 12:50:43 +0000

    Updated the dataconnection class to the right version

---
+++ dbo.tblstaff.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[tblstaff] (
+    [employee_id] INT           IDENTITY (1, 1) NOT NULL,
+	[salary] INT NOT NULL,
+    [first_name]  VARCHAR (255) NOT NULL,
+    [last_name]   VARCHAR (255) NOT NULL,
+    [active]      BIT           DEFAULT ((1)) NOT NULL,
+    [created_at]  DATETIME      DEFAULT (getdate()) NOT NULL,
+    PRIMARY KEY CLUSTERED ([employee_id] ASC)
+);
+
+
+
