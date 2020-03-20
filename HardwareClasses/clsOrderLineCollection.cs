a4f12bc00d0883c16a9ecdd7ce404c7e9385010e
Steven Ginns <steven.ginns@gmail.com>
2020-03-20 19:36:27 +0000

    Collection classes created

---
+++ HardwareClasses/clsOrderLineCollection.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HardwareClasses
+{
+    public class clsOrderLineCollection
+    {
+        private List<clsOrderLine> mOrderList = new List<clsOrderLine>();
+        private int mCount;
+        public List<clsOrderLine> orderLineList { get { return mOrderList; } set { mOrderList = value; } }
+        public int Count { get { return mCount; } set { mCount = value; } }
+        public clsOrderLine ThisOrderLine { get; set; }
+
+        public clsOrderLineCollection()
+        {
+            Int32 index = 0;
+
+            Int32 recordCount = 0;
+
+            clsDataConnection DB = new clsDataConnection();
+
+            DB.Execute("sproc_tblOrderLine_SelectAll");
+
+            recordCount = DB.Count;
+
+            while (index < recordCount) 
+            {
+                clsOrderLine testItem = new clsOrderLine
+                {
+                    OrderId = Convert.ToInt32(DB.DataTable.Rows[index]["OrderId"]),
+                    OrderLineId = Convert.ToInt32(DB.DataTable.Rows[index]["OrderLineId"]),
+                    Quantity = Convert.ToInt32(DB.DataTable.Rows[index]["Quantity"]),
+                    ProductId = Convert.ToInt32(DB.DataTable.Rows[index]["ProductId"])
+                };
+
+                mOrderList.Add(testItem);
+
+                index++;
+            }
+        }
+    }
+}
---
+++ HardwareFrontEnd/OrderList.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Web.UI;
+using HardwareClasses;
+
+public partial class OrderList : Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if(IsPostBack == false) 
+        {
+            DisplayOrders();
+        }
+    }
+
+    void DisplayOrders() 
+    {
+        clsOrderCollection orders = new clsOrderCollection();
+
+        lstOrderList.DataSource = orders.orderList;
+
+        lstOrderList.DataValueField = "OrderId";
+
+        lstOrderList.DataBind();
+    }
+}
